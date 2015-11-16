249cc5f7986bedb1ce6eff6ebde62cde4f407d21
AB <andreabuffasbru@gmail.com>
2015-11-16 23:07:07 +0100

    third..

--- main.cpp
+++ main.cpp
@@ -6,13 +6,18 @@ int main ()
 {
   UVector<int> activeUser(0, 10);
   activeUser.set(0, 1);
-  //activeUser.set(3, 1);
+  activeUser.set(3, 1);
   std::cout << "Active User profile:\n" << activeUser.get(0) << std::endl;
 
   UIMatrix users(0, 0, 10);
   users.set(0, 0, 1);
-  //users.set(0, 3, 1);
+  users.set(0, 3, 1);
   users.set(1, 1, 1);
+  users.set(1, 2, 1);
+
+  users.set(2, 0, 1);
+  users.set(2, 1, 1);
+  users.set(2, 2, 1);
 
   //int res[3] = {0, 0, 0};
   UVector<int*> res(0, 10);
@@ -21,8 +26,8 @@ int main ()
   double cosineDistance = 0.0;
   for(UVector<int*>::iterator it = res.begin(); it != res.end(); it++) {
     if (it->second[1] != 0 && it->second[2] != 0) {
-      //std::cout << "TEST" << it->first << std::endl;
-      cosineDistance = (double)it->second[0] / sqrt((double)(it->second[1]) * sqrt((double)it->second[2]));
+      //std::cout << "TEST" << it->second[0]  << std::endl;
+      cosineDistance = it->second[0] / (sqrt(it->second[1]) * sqrt(it->second[2]));
     } else {
       cosineDistance = 0.0;
     }
